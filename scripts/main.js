1b656805ea73e7a394324d31030ee742c4ec0ede
Sadaffi <bartek_sadowski@o2.pl>
2019-03-11 14:53:05 +0100

    player state is now in object

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,27 @@
-let score = +($("#score").text()); //how much money
-let power = 1; //how much do I get on click of buttons
-let numberOfMiners = 2; //how many minersfields there are
-let miners = []; //miners list
+
 let dpu = [1, 5]; //dig per miner for next indexes of miners
 let cost = [10, 100]; //costs for buying new miner
 
+let ps = { //player state
+    score: 0, //how much money
+    power: 1, //how much do I get on click of buttons
+    numberOfMiners: 2, //how many minersfields there are
+    miners: [], //miners list
+};
+
 //initiating miners
-for(let i = 0; i < numberOfMiners; i++){
-    miners[i]=new Miner(dpu[i], cost[i]);
+for(let i = 0; i < ps.numberOfMiners; i++){
+    ps.miners[i]=new Miner(dpu[i], cost[i]);
 }
 
 //handling mining by hand
-$("#increment").click(()=>{upScore(power)});
+$("#increment").click(()=>{upScore(ps.power)});
 
 //handling mining by miners
 function minershandler(){
     let income = 0;
-    for(let i = 0; i < numberOfMiners; i++){
-        income += miners[i].getIncome();
+    for(let i = 0; i < ps.numberOfMiners; i++){
+        income += ps.miners[i].getIncome();
     }
     upScore(income);
 }
@@ -27,8 +31,8 @@ setInterval(minershandler, 1000);
 
 //adding some amount to score
 function upScore(ammount){
-    score += ammount;
-    $("#score").text(score);
+    ps.score += ammount;
+    $("#score").text(ps.score);
 }
 
 //miners buying
@@ -39,11 +43,11 @@ $(".miner").click(function(){
 //handling miners buying
 function handleBuying(id){
     id = +(id.substring(5)); //getting the id of miner clicked
-    if(score >= miners[id].cost){ //chceking if you have enough of money
-        upScore(-miners[id].cost);
-        miners[id].buyMe();
-        $("#miner"+id).find(".ammount").text(miners[id].units); //updating units shown to player
-        $("#miner"+id).find(".cost").text(miners[id].cost); //updating cost shown to player
-        $("#miner"+id).find(".income").text(miners[id].getIncome()); //updating income shown to player
+    if(ps.score >= ps.miners[id].cost){ //chceking if you have enough of money
+        upScore(-ps.miners[id].cost);
+        ps.miners[id].buyMe();
+        $("#miner"+id).find(".ammount").text(ps.miners[id].units); //updating units shown to player
+        $("#miner"+id).find(".cost").text(ps.miners[id].cost); //updating cost shown to player
+        $("#miner"+id).find(".income").text(ps.miners[id].getIncome()); //updating income shown to player
     }
 }
