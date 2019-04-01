573465122720f421d8ba25d7f04d1d0ee807b779
Sadaffi <bartek_sadowski@o2.pl>
2019-04-01 23:21:35 +0200

    Now upgrades can be bought, and affect player and miners

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const initVM = {1:10, 5:100, 15: 1000, 50:10000}; //initial dig and cost for each miner
-const upgds = {First:[100, {0: 1}], Second:[200, {1:1}], Third:[300, {1:1}]}; //upgrades with structure name:[cost, {target: ammount}]
+const upgds = {First:[5, 0, {0: 1, 1:2}], Second:[200, 0, {2:1}], Third:[300, 0, {3:1}]}; //upgrades with structure name:[cost, {target: ammount}]
 
 let ps = { //player state
     score: 0, //how much money
@@ -32,18 +32,35 @@ if(localStorage.getItem('playerstate')!=null) {  //checking if previous state ex
 }
 updateClickPower(0);
 
+console.log(ps.upgradez)
+
+//creating upgrades buttons
+for(let i=0; i<ps.upgradez.length; i++){
+    if(!ps.upgradez[i].bought){
+        $(".upgradesList").append(`
+            <button class=upgrade id=upgrade${i}>
+            ${ps.upgradez[i].cost}
+            </button>
+        `);
+    }
+}
+
+//creating miners buttons
+for(let i=0; i<ps.miners.length; i++){
+    $(".mines").append(`
+        <button class="miner" id="miner${i}">
+        <p>#${i+1} : <span class="ammount">0</span></p>
+        <p>Total income: <span class="income">0</span></p>
+        <p>Buy new: <span class="cost">${ps.miners[i].cost}<span></p>
+        </button>
+    `);
+}
+
 //showing updated values on buttons
 function updateMiners(id) {
     $("#miner"+id).find(".ammount").text(ps.miners[id].units); //updating units shown to player
     $("#miner"+id).find(".cost").text(ps.miners[id].cost); //updating cost shown to player
     $("#miner"+id).find(".income").text(ps.miners[id].getIncome); //updating income shown to player
-}
-
-//creating upgrades buttons
-for(let i=0; i<ps.upgradez.length; i++){
-    if(!ps.upgradez[i].bought){
-        $(".upgradesList").append(`<button class=upgrade>${ps.upgradez[i].cost}</button>`)
-    }
 }
 
 //power update
@@ -64,8 +81,27 @@ function upScore(ammount){
     $("#score").text(ps.score);
 }
 
+//handling upgrades buying
+$(".upgrade").click((event)=>{
+    id = +($(event.currentTarget).attr("id").substring(7));
+    if(ps.score >= ps.upgradez[id].cost){
+        upScore(-ps.upgradez[id].cost);
+        pairs=Object.entries(ps.upgradez[id].buyMe());
+        for(pair of pairs){
+            if(pair[0]==0){
+                updateClickPower(pair[1]);
+            }else{
+                id=pair[0]-1;
+                ps.miners[id].newDpu(pair[1]);
+                updateMiners(id);
+            }
+        }
+
+    }
+});
+
 //handling miners buying
-$(".miner").click( (event) =>{
+$(".miner").click((event)=>{
     id = +($(event.currentTarget).attr("id").substring(5));
     if(ps.score >= ps.miners[id].cost){ //chceking if you have enough of money
         upScore(-ps.miners[id].cost);
--- scripts/miners.js
+++ scripts/miners.js
@@ -1,12 +1,17 @@
 class Miner{
     constructor(units, digperunit, cost){
         this.units = units;
-        this.dpu = digperunit;
+        this.dpu = +digperunit;
         this.cost = cost;
     }
 
+    newDpu(dpuN){
+        this.dpu = this.dpu + dpuN;
+        return this.dpu;
+    }
+
     get getIncome(){
-        return(this.units*this.dpu)
+        return(this.units * this.dpu)
     }
 
     buyMe(){
